95900244db771e30b2cce04428635193e2bb08b7
NoNameA 774 <nonamea774@gmail.com>
2014-04-22 00:48:12 +0900

    いろいろ設定できるようにしました

--- haruna.rb
+++ haruna.rb
@@ -5,31 +5,55 @@ require 'uri'
 require 'nokogiri'
 
 Plugin.create(:harunahadaijoubu) do
-    def haruna(service)
+  UserConfig[:harunahadaijoubu_timeout] ||= 10
+  UserConfig[:harunahadaijoubu_name] ||= "榛名"
+  UserConfig[:harunahadaijoubu_shindan] ||= "344032"
+  UserConfig[:harunahadaijoubu_hour] ||= 0
+  UserConfig[:harunahadaijoubu_minute] ||= 3
+  
+  settings "榛名の時間" do
+    boolean '有効', :harunahadaijoubu_enable
+    adjustment("タイムアウト(sec)", :harunahadaijoubu_timeout, 0, 60)
+    input("診断番号", :harunahadaijoubu_shindan)
+    input("診断する名前", :harunahadaijoubu_name)
+    adjustment("発火時間(時)", :harunahadaijoubu_hour, 0, 23)
+    adjustment("発火時間(分)", :harunahadaijoubu_minute, 0, 59)
+  end
+  def haruna(service)
+    begin 
       Net::HTTP.start('shindanmaker.com') do |http|
-        haruan_shindan = 344032
-        haruna_string = "榛名"
-        res = http.post("/#{haruan_shindan}", "u=#{haruna_string}")
+        http.read_timeout = UserConfig[:harunahadaijoubu_timeout].to_i
+        res = http.post("/#{UserConfig[:harunahadaijoubu_shindan]}", "u=#{UserConfig[:harunahadaijoubu_name]}")
         doc = Nokogiri::HTML::parse(res.body)
         txt = doc.xpath('//textarea').first.inner_text
         service.update(:message => txt)
       end
+    rescue
+      notice "timeout! @harunatime"
+      Plugin.activity(:error, "榛名は大丈夫じゃないです。")
     end
-    # 次回のよるほー時間を取得
-    def harunatime
-      now = Time.new
-      result = Time.local(now.year, now.month, now.day, 0, 3)
-      while result < now
-        result += 86400 end
-      result end
-    puts "haruna"
-    puts harunatime
-    Reserver.new(harunatime){
-      haruna(Service.primary)
-      puts "haruna Called"
-    }
-
   end
+
+  def harunatime
+    now = Time.new
+    result = Time.local(now.year, now.month, now.day, UserConfig[:harunahadaijoubu_hour], UserConfig[:harunahadaijoubu_minute])
+    while result < now
+      result += 86400 end
+    result end
+
+  def main 
+    if UserConfig[:harunahadaijoubu_enable] 
+      Reserver.new(harunatime){
+        haruna(Service.primary)
+        sleep 1
+        main
+      }
+    end
+  end
+  
+  main
+  
+end
 
 
 
